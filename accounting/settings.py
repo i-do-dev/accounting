12bf91cc8da164fb04dbce5f5b05ccd7f363906c
Waqar Muneer <i.do.dev@outlook.com>
2024-01-28 16:26:31 +0500

    upate allowed hosts

--- accounting/settings.py
+++ accounting/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'ec2-52-66-138-84.ap-south-1.compute.amazonaws.com']
 
 
 # Application definition
