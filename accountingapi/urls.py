e6d6c26675d892a5c97458d1b8c83ffffe36721e
Waqar Muneer <i.do.dev@outlook.com>
2024-01-30 23:27:50 +0500

    url rewrite

--- accountingapi/urls.py
+++ accountingapi/urls.py
@@ -4,16 +4,16 @@ from .views import *
 
 router = DefaultRouter()
 router.register(r'accounts', AccountViewSet)
-router.register(r'accountTags', AccountTagViewSet)
-router.register(r'accountTypes', AccountTypeViewSet)
-router.register(r'accountTaxes', AccountTaxViewSet)
-router.register(r'resCompanies', ResCompanyViewSet)
-router.register(r'accountJournals', AccountJournalViewSet)
-router.register(r'accountMoveLines', AccountMoveLineViewSet)
-router.register(r'accountMoves', AccountMoveViewSet)
-router.register(r'accountGroups', AccountGroupViewSet)
+router.register(r'account-tags', AccountTagViewSet)
+router.register(r'account-types', AccountTypeViewSet)
+router.register(r'account-taxes', AccountTaxViewSet)
+router.register(r'companies', ResCompanyViewSet)
+router.register(r'account-journals', AccountJournalViewSet)
+router.register(r'account-moveLines', AccountMoveLineViewSet)
+router.register(r'account-moves', AccountMoveViewSet)
+router.register(r'account-groups', AccountGroupViewSet)
 router.register(r'reports', ReportViewSet)
-router.register(r'accountCategories', AccountCategoryViewSet)
+router.register(r'account-categories', AccountCategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
